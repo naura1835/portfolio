1972fa693ea87efe1645406c9ea33558ced89a32
Khadija Gwarzo <khadijagarbag@gmail.com>
2023-10-18 19:27:44 +0100

    update about me

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -25,28 +25,27 @@ const About = () => {
     >
       <Description>
         Hello there! I am a front end developer who loves building captivating
-        user interfaces that keep users engaged while offering reliable
-        solutions from concept to design and finally development phase. I'm
-        always striving to learn more, improve my skills and gain experiences
-        while putting what i've learnt into action.
+        user interfaces that keep users engaged. My passion for front-end
+        development stems from interacting with beautifully crafted websites. I
+        love hopping on websites and seeing the effort put by other developers
+        to give a user an immersive experience. Utilizing my skills i want to
+        give other users that same experience thereby making going through a
+        website a fun and engaging one.
       </Description>
       <Description>
         While programming is my first love, I also have an eye for design. I
         love to dabble in creating user interfaces and experimenting with
         different color schemes and layouts. I often find myself browsing
-        through websites just to admire their design and functionality.
+        through websites just to admire their design and functionality. Also fun
+        fact i design all my individual projects myself giving customers the
+        whole package.
       </Description>
       <Description>
         When I'm not writing code or designing UIs, I love to explore my other
-        passion which is baking. There's something magical about mixing
+        hobby which is baking. There's something magical about mixing
         ingredients together and watching them transform into delicious treats
         (yes i have a sweet tooth).
       </Description>
-      {/* <Description>
-        I am excited to continue to learn, explore, and create with programming,
-        design, and baking. Thank you for taking the time to learn a bit about
-        me.
-      </Description> */}
     </Wrapper>
   );
 };

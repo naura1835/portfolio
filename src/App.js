56ae12ef87b11eb0784dd3cd9e65725f8e7f0662
Khadija Gwarzo <khadijagarbag@gmail.com>
2023-10-31 15:52:15 +0100

    fix navbar bug in header, remove unused files, update about section and style

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Header from "./components/header/header.component";
 import HeroSection from "./components/hero-section/hero-section.component";
 import ProjectOverview from "./components/project-overview/project-overview.component";
 import Section from "./components/section/section.component";
-import Services from "./components/services/services.component";
-import TechStack from "./components/tech-stack/tech-stack.components";
 
 import "./App.css";
 
@@ -42,27 +40,8 @@ const App = () => {
         <ProjectOverview />
 
         <div id="about" />
-        <Section
-          iconSrc="icons/line-md_account-small.svg"
-          altText="account icon"
-          title="About"
-        >
-          <About />
-        </Section>
-        <Section
-          iconSrc="icons/et_gears.svg"
-          altText="folder icon"
-          title="Services"
-        >
-          <Services />
-        </Section>
-        <Section
-          iconSrc="icons/ph_stack-light.svg"
-          altText="stack icon"
-          title="Tech stack"
-        >
-          <TechStack />
-        </Section>
+        <About />
+
         <div id="contact" />
         <Section
           iconSrc="icons/fluent_contact-card-20-regular.svg"
